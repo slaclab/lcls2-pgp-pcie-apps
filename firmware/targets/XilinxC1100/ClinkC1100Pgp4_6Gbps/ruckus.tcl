e71ce81879f110cfb931706a3f336cb9091ebfe4
Larry Ruckman <ruckman@slac.stanford.edu>
2022-03-21 16:36:46 -0700

    adding ClinkC1100Pgp4_6Gbps target

---
+++ firmware/targets/XilinxC1100/ClinkC1100Pgp4_6Gbps/ruckus.tcl
@@ -0,0 +1,19 @@
+# Load RUCKUS environment and library
+source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
+
+# Load base sub-modules
+loadRuckusTcl $::env(TOP_DIR)/submodules/surf
+loadRuckusTcl $::env(TOP_DIR)/submodules/lcls-timing-core
+loadRuckusTcl $::env(TOP_DIR)/submodules/axi-pcie-core/hardware/XilinxVariumC1100
+loadRuckusTcl $::env(TOP_DIR)/submodules/lcls2-pgp-fw-lib/hardware/XilinxVariumC1100
+
+# Load the l2si-core source code
+loadSource -lib l2si_core -dir "$::env(TOP_DIR)/submodules/l2si-core/xpm/rtl"
+loadSource -lib l2si_core -dir "$::env(TOP_DIR)/submodules/l2si-core/base/rtl"
+
+# Load local source Code
+loadSource -dir "$::DIR_PATH/hdl"
+loadConstraints -dir "$::DIR_PATH/hdl"
+
+# Updating impl_1 strategy
+set_property strategy Performance_ExplorePostRoutePhysOpt [get_runs impl_1]
