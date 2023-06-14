9d16a30f4362db047a67f5509bcc49b97ce54d03
Larry Ruckman <ruckman@slac.stanford.edu>
2023-06-14 12:46:26 -0700

    updating code headers

--- firmware/targets/XilinxC1100/Lcls2XilinxC1100Pgp4_6Gbps/vivado/promgen.tcl
+++ firmware/targets/XilinxC1100/Lcls2XilinxC1100Pgp4_6Gbps/vivado/promgen.tcl
@@ -1,9 +1,9 @@
 ##############################################################################
-## This file is part of 'Camera link gateway'.
+## This file is part of 'lcls2-pgp-pcie-apps'.
 ## It is subject to the license terms in the LICENSE.txt file found in the
 ## top-level directory of this distribution and at:
 ##    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
-## No part of 'Camera link gateway', including this file,
+## No part of 'lcls2-pgp-pcie-apps', including this file,
 ## may be copied, modified, propagated, or distributed except according to
 ## the terms contained in the LICENSE.txt file.
 ##############################################################################
